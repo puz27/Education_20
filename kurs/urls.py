db420678049b8144f6532039676e98e326a1bcfc
anv <anv@woori.ru>
2023-07-11 17:33:00 +0300

    add URLS

--- kurs/urls.py
+++ kurs/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from kurs.views.course import CourseViewSet
 from kurs.views.lesson import LessonDetailView, LessonListView, LessonCreateView, LessonDeleteView, LessonUpdateView
 
-app = "kurs"
+app_name = "cource"
 
 urlpatterns = [
-    path("lesson/", LessonListView.as_view()),
-    path("lesson/<int:pk>", LessonDetailView.as_view()),
-    path("lesson/<int:pk>", LessonCreateView.as_view()),
-    path("lesson/<int:pk>/update", LessonUpdateView.as_view()),
-    path("lesson/<int:pk>/delete", LessonDeleteView.as_view()),
+    path("", LessonListView.as_view()),
+    path("<int:pk>", LessonDetailView.as_view()),
+    path("<int:pk>/create", LessonCreateView.as_view()),
+    path("<int:pk>/update", LessonUpdateView.as_view()),
+    path("<int:pk>/delete", LessonDeleteView.as_view()),
 ]
