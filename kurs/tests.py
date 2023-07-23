31e200bee71f7a19781c3e37e82eabcc388a9e07
anv <anv@woori.ru>
2023-07-23 21:54:04 +0300

    add coverage

--- kurs/tests.py
+++ kurs/tests.py
@@ -4,136 +4,8 @@ from rest_framework import status
 from users.models import Users
 
 
-class LessonTestCase(APITestCase):
-    """ Testing Lessons """
-
-    def setUp(self):
-        """ Prepare testing """
-        self.user = Users(
-            email="test3@gmail.com"
-        )
-        self.user.set_password("test3")
-        self.user.save()
-
-        response = self.client.post(
-            "/api/v1/users/token/",
-            {"email": "test3@gmail.com", "password": "test3"}
-        )
-
-        self.access_token = response.json().get("access")
-        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {self.access_token}")
-
-        self.course = Course.objects.create(
-            title="Test Course",
-            description="Test Course description",
-        )
-
-        self.lesson = Lesson.objects.create(
-            title="Test Lesson",
-            description="Test Lesson description",
-            course=self.course
-        )
-
-    def test_get_lessons(self):
-        response = self.client.get("/api/v1/lesson/")
-
-        self.assertEqual(
-            response.status_code,
-            status.HTTP_200_OK
-        )
-
-    def test_create_lesson(self):
-        data = {"title": "Test title",
-                "description": "Test description"
-                }
-
-        response = self.client.post("/api/v1/lesson/create/", data)
-
-        self.assertEqual(
-            response.status_code,
-            status.HTTP_201_CREATED
-        )
-        self.assertTrue(Lesson.objects.all().exists())
-
-    def test_validate_lesson(self):
-        data = {"title": "Test Lesson23",
-                "description": "https://vk.com"
-                }
-
-        response = self.client.post("/api/v1/lesson/create/", data)
-
-        self.assertEqual(
-            response.status_code,
-            status.HTTP_400_BAD_REQUEST
-        )
-
-
-class CourseTestCase(APITestCase):
-    """ Testing Courses """
-
-    def setUp(self):
-        """ Prepare testing """
-        self.user = Users(
-            email="test3@gmail.com"
-        )
-        self.user.set_password("test3")
-        self.user.save()
-
-        response = self.client.post(
-            "/api/v1/users/token/",
-            {"email": "test3@gmail.com", "password": "test3"}
-        )
-
-        self.access_token = response.json().get("access")
-        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {self.access_token}")
-
-        self.course = Course.objects.create(
-            title="Test Course",
-            description="Test Course description",
-        )
-
-        self.lesson = Lesson.objects.create(
-            title="Test Lesson",
-            description="Test Lesson description",
-            course=self.course
-        )
-
-    def test_get_courses(self):
-            response = self.client.get("/api/v1/course/")
-
-            self.assertEqual(
-                response.status_code,
-                status.HTTP_200_OK
-            )
-
-    def test_create_course(self):
-        data = {"title": "Test title",
-                "description": "Test description"
-                }
-
-        response = self.client.post("/api/v1/course/create/", data)
-
-        self.assertEqual(
-            response.status_code,
-            status.HTTP_201_CREATED
-        )
-        self.assertTrue(Course.objects.all().exists())
-
-    def test_validate_course(self):
-            data = {"title": "Test Lesson23",
-                    "description": "https://vk.com"
-                    }
-
-            response = self.client.post("/api/v1/course/create/", data)
-
-            self.assertEqual(
-                response.status_code,
-                status.HTTP_400_BAD_REQUEST
-            )
-
-
-class SubscriptionTestCase(APITestCase):
-    """ Testing Subscription """
+class KursTestCase(APITestCase):
+    """ Testing Application KURS """
 
     def setUp(self):
         """ Prepare testing """
@@ -163,12 +35,13 @@ class SubscriptionTestCase(APITestCase):
         )
 
     def test_create_subscription(self):
-        data = {"owner": 1,
+        data = {'id': 1,
+                'is_subscribed': False,
+                "owner": 1,
                 "course": 1
                 }
 
         response = self.client.post("/api/v1/subscription/create/", data)
-
         self.assertEqual(
             response.status_code,
             status.HTTP_201_CREATED
@@ -182,4 +55,174 @@ class SubscriptionTestCase(APITestCase):
             response.status_code,
             status.HTTP_200_OK
         )
+
+    def test_get_lessons(self):
+            response = self.client.get("/api/v1/lesson/")
+
+            self.assertEqual(
+                response.status_code,
+                status.HTTP_200_OK
+            )
+
+    def test_validate_lesson(self):
+        data = {"title": "Test Lesson23",
+                "description": "https://vk.com"
+                }
+
+        response = self.client.post("/api/v1/lesson/create/", data)
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_400_BAD_REQUEST
+        )
+
+    def test_get_courses(self):
+            response = self.client.get("/api/v1/course/")
+
+            self.assertEqual(
+                response.status_code,
+                status.HTTP_200_OK
+            )
+
+    def test_validate_course(self):
+            data = {"title": "Test Lesson23",
+                    "description": "https://vk.com"
+                    }
+
+            response = self.client.post("/api/v1/course/create/", data)
+
+            self.assertEqual(
+                response.status_code,
+                status.HTTP_400_BAD_REQUEST
+            )
+
+
+# class CourseTestCase(APITestCase):
+#     """ Testing Courses """
+#
+#     def setUp(self):
+#         """ Prepare testing """
+#         self.user = Users(
+#             email="test3@gmail.com"
+#         )
+#         self.user.set_password("test3")
+#         self.user.save()
+#
+#         response = self.client.post(
+#             "/api/v1/users/token/",
+#             {"email": "test3@gmail.com", "password": "test3"}
+#         )
+#
+#         self.access_token = response.json().get("access")
+#         self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {self.access_token}")
+#
+#         self.course = Course.objects.create(
+#             title="Test Course",
+#             description="Test Course description",
+#         )
+#
+#         self.lesson = Lesson.objects.create(
+#             title="Test Lesson",
+#             description="Test Lesson description",
+#             course=self.course
+#         )
+#
+#     def test_get_courses(self):
+#             response = self.client.get("/api/v1/course/")
+#
+#             self.assertEqual(
+#                 response.status_code,
+#                 status.HTTP_200_OK
+#             )
+#
+#     def test_create_course(self):
+#         data = {"title": "Test title",
+#                 "description": "Test description"
+#                 }
+#
+#         response = self.client.post("/api/v1/course/create/", data)
+#
+#         self.assertEqual(
+#             response.status_code,
+#             status.HTTP_201_CREATED
+#         )
+#         self.assertTrue(Course.objects.all().exists())
+#
+#     def test_validate_course(self):
+#             data = {"title": "Test Lesson23",
+#                     "description": "https://vk.com"
+#                     }
+#
+#             response = self.client.post("/api/v1/course/create/", data)
+#
+#             self.assertEqual(
+#                 response.status_code,
+#                 status.HTTP_400_BAD_REQUEST
+#             )
+# class LessonTestCase(APITestCase):
+#     """ Testing Lessons """
+#
+#     def setUp(self):
+#         """ Prepare testing """
+#         self.user = Users(
+#             email="test3@gmail.com"
+#         )
+#         self.user.set_password("test3")
+#         self.user.save()
+#
+#         response = self.client.post(
+#             "/api/v1/users/token/",
+#             {"email": "test3@gmail.com", "password": "test3"}
+#         )
+#
+#         self.access_token = response.json().get("access")
+#         self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {self.access_token}")
+#
+#         self.course = Course.objects.create(
+#             title="Test Course",
+#             description="Test Course description",
+#         )
+#
+#         self.lesson = Lesson.objects.create(
+#             title="Test Lesson",
+#             description="Test Lesson description",
+#             course=self.course
+#         )
+#
+#     def test_get_lessons(self):
+#         response = self.client.get("/api/v1/lesson/")
+#
+#         self.assertEqual(
+#             response.status_code,
+#             status.HTTP_200_OK
+#         )
+#
+#     def test_create_lesson(self):
+#         data = {"title": "Test title",
+#                 "description": "Test description"
+#                 }
+#
+#         response = self.client.post("/api/v1/lesson/create/", data)
+#
+#         self.assertEqual(
+#             response.status_code,
+#             status.HTTP_201_CREATED
+#         )
+#         self.assertTrue(Lesson.objects.all().exists())
+#
+#     def test_validate_lesson(self):
+#         data = {"title": "Test Lesson23",
+#                 "description": "https://vk.com"
+#                 }
+#
+#         response = self.client.post("/api/v1/lesson/create/", data)
+#
+#         self.assertEqual(
+#             response.status_code,
+#             status.HTTP_400_BAD_REQUEST
+#         )
+
+
+
+
 
