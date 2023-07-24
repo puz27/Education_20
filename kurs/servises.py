7b6e9fabb0283f15ed5924795ff8e94333e35546
Nick_Linux <n.avramenko87@gmail.com>
2023-07-24 23:55:44 +0300

    add POST

--- kurs/servises.py
+++ kurs/servises.py
@@ -1,5 +1,7 @@
 import requests
 import stripe
+from rest_framework import status
+from rest_framework.decorators import api_view
 
 # make_payment = "https://stripe.com/docs/api/payment_intents/create"
 # get_payment = "https://stripe.com/docs/api/payment_intents/retrieve"
@@ -16,11 +18,19 @@ stripe.PaymentIntent.create(
   automatic_payment_methods={"enabled": True},
 )
 """
-make_payment = "https://stripe.com/docs/api/payment_intents#create_payment_intent"
-response = requests.get(make_payment)
-print(response.content)
+# make_payment = "https://stripe.com/docs/api/payment_intents#create_payment_intent"
+# response = requests.get(make_payment)
+# print(response.content)
 
-# stripe.api_key = "sk_test_51NXPaKCn4C5dva66mINywzPgyNFznygCyoFq01lCmrEHwkmEGzEFLfp36l1Nzx1Gt9jJxCOcrfbKY9R3HCxSfkjQ00NkNSsW8K"
+# params = {
+#                 "text": keywords,
+#                 "per_page": 100,
+#                 "page": page_number,
+#                         }
+
+# response = requests.get(make_payment, params=params)
+#
+stripe.api_key = "sk_test_51NXPaKCn4C5dva66mINywzPgyNFznygCyoFq01lCmrEHwkmEGzEFLfp36l1Nzx1Gt9jJxCOcrfbKY9R3HCxSfkjQ00NkNSsW8K"
 #
 # stripe.PaymentIntent.create(
 #   amount=2000,
@@ -29,3 +39,16 @@ print(response.content)
 # )
 #
 # print(stripe)
+
+
+@api_view(['POST'])
+def test_payment(request):
+    test_payment_intent = stripe.PaymentIntent.create(
+      amount=1000,
+      currency='USD',
+      payment_method_types=['card'],
+      receipt_email='test@example.com')
+    return request.Response(status=status.HTTP_200_OK, data=test_payment_intent)
+
+
+test_payment()
